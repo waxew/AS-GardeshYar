1a7921ab51908cc95b472c7564190cbcfd06a219
Alex Garneau <alex@gskinner.com>
2026-08-26 15:59:50 -0600

    Removed Focus elements throughout the app (those hijack the tabbing). Need to ensure the VoiceOver components aren't affected).

--- lib/ui/screens/editorial/widgets/_callout.dart
+++ lib/ui/screens/editorial/widgets/_callout.dart
@@ -6,21 +6,19 @@ class _Callout extends StatelessWidget {
   const _Callout({super.key, required this.text});
   @override
   Widget build(BuildContext context) {
-    return Focus(
-      child: IntrinsicHeight(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(color: $styles.colors.accent1, width: 1),
-            Gap($styles.insets.sm),
-            Expanded(
-              child: Text(
-                text,
-                style: $styles.text.callout,
-              ),
+    return IntrinsicHeight(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(color: $styles.colors.accent1, width: 1),
+          Gap($styles.insets.sm),
+          Expanded(
+            child: Text(
+              text,
+              style: $styles.text.callout,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
